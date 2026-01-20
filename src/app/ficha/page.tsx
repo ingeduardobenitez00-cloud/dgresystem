442d668c499585e88f2b410cd83ed7c5a0859aa4
Ing. Eduardo Benitez <ing.eduardobenitez00@gmail.com>
2026-01-20 10:34:45 +0000

    mejor baja el boton de generar pdf debajo de editar informe

--- src/app/ficha/page.tsx
+++ src/app/ficha/page.tsx
@@ -607,7 +607,7 @@ export default function FichaPage() {
                             </CardTitle>
                             <CardDescription>{selectedDepartment} - {selectedDistrict}</CardDescription>
                           </div>
-                          <div className="flex gap-2">
+                          <div className="flex flex-col gap-2">
                              {canEditReport && (
                                <Button onClick={() => setEditModalOpen(true)} variant="outline" size="sm">
                                   <Edit className="mr-2 h-4 w-4" />
