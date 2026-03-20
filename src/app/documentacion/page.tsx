74f5894b0836ab80004cf634dd437804388124f2
Ing. Eduardo Benitez <ing.eduardobenitez00@gmail.com>
2026-03-20 15:59:35 +0000

    Try fixing this error: `Unhandled Runtime Error: ReferenceError: Calenda

--- src/app/documentacion/page.tsx
+++ src/app/documentacion/page.tsx
@@ -37,7 +37,8 @@ import {
   X,
   PlusCircle,
   Save,
-  QrCode
+  QrCode,
+  CalendarDays
 } from 'lucide-react';
 import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
