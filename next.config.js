f62a45b9ce0d31790a376f502e9e6745ed265afc
Ing. Eduardo Benitez <ing.eduardobenitez00@gmail.com>
2026-03-28 16:23:30 +0000

    porque me sale este error 6000-firebase-studio-1761738264865.cluster-lqz

--- next.config.js
+++ next.config.js
@@ -1,19 +1,17 @@
-
 /** @type {import('next').NextConfig} */
 const cspHeader = `
     default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline' https://apis.google.com https://www.gstatic.com;
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' https://apis.google.com https://www.gstatic.com https://*.cloudworkstations.dev;
     style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://unpkg.com;
     img-src 'self' blob: data: https://images.unsplash.com https://picsum.photos https://api.qrserver.com https://placehold.co https://*.googleusercontent.com https://*.gstatic.com;
     font-src 'self' https://fonts.gstatic.com data:;
     object-src 'none';
     base-uri 'self';
     form-action 'self';
-    frame-ancestors 'none';
+    frame-ancestors 'self' https://*.cloudworkstations.dev;
     frame-src 'self' https://*.firebaseapp.com;
-    connect-src 'self' https://*.googleapis.com https://*.firebaseio.com https://*.firebaseapp.com wss://*.firebaseio.com;
+    connect-src 'self' https://*.googleapis.com https://*.firebaseio.com https://*.firebaseapp.com wss://*.firebaseio.com wss://*.cloudworkstations.dev https://*.cloudworkstations.dev;
     block-all-mixed-content;
-    upgrade-insecure-requests;
 `.replace(/\s{2,}/g, ' ').trim();
 
 const nextConfig = {
